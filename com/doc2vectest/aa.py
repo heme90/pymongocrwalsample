54d9c6b6da275db476211ee3213ea1f7e11fc352
heme90 <dbsrl0423@gmail.com>
2019-05-28 18:25:54 +0900

    20190528

--- com/doc2vectest/aa.py
+++ com/doc2vectest/aa.py
@@ -6,7 +6,6 @@ Created on 2019. 4. 29.
 import gensim
 
 #저장한 모델을 불러옵니다
-md = gensim.models.Doc2Vec.load("test.model")
+md = gensim.models.Doc2Vec.load("jcjc.model")
 print(md.wv.similar_by_word("문재인"))
-print(md.wv.most_similar("트럼프"))
 print(md.docvecs)
